fa2ce3f3e461ad1d1b5e130a17f3771c1818868d
Barry McMillan <barry.mcmillan@gmail.com>
2024-08-20 17:31:49 +0100

    Initial stack: SolidJS, Tailwind, TS, DaisyUI

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="flex flex-col justify-center items-center space-y-8 py-8">
+      <p class="text-4xl">Hello, world!</p>      
+      <button class="btn">Button</button>
+    </div>
+  );
+};
+
+export default App;
