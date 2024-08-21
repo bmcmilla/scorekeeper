95f9df47fb1bab818363880a5838c4c27706f5fd
Barry McMillan <barry.mcmillan@gmail.com>
2024-08-21 17:51:50 +0100

    Center dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     const [user, setUser] = createSignal({});
 
     return (
-        <div class="m-8">
+        <div class="flex flex-col justify-center items-center m-8">
             <h4 class="mb-6">Welcome, User</h4>
             <button type="button" class="btn btn-primary">Sign Out</button>
         </div>
