62efafa2e0b572d27f70bb1247c28f3078ec75bb
Barry McMillan <barry.mcmillan@gmail.com>
2024-08-22 11:20:17 +0100

    Supabase environment

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@
 import './index.css';
 import { render } from 'solid-js/web';
 import { Router } from "@solidjs/router";
+import { createClient } from '@supabase/supabase-js';
+import { SupabaseProvider } from 'solid-supabase';
 
 import App from './App';
 
 const root = document.getElementById('root');
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY);
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
@@ -13,9 +17,10 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   );
 }
 
-
 render(() =>
-  <Router>
-    <App />
-  </Router>
+  <SupabaseProvider client={supabase}>
+    <Router>
+      <App />
+    </Router>
+  </SupabaseProvider>
   , root!);
