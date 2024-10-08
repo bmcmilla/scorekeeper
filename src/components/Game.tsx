94c73c5a1180f3ec5e786fa55083da7ae51f769b
Barry McMillan <barry.mcmillan@gmail.com>
2024-10-09 00:04:24 +0100

    SVG icons on game view

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -158,7 +158,10 @@ function Game() {
                 <div class="stat-value">{gameData.countRounds()} </div>
                 <div class="stat-actions">
                   <button class="btn btn-sm btn-primary" onClick={newRound}>
-                    + Round {gameData.countRounds() + 1}
+                    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
+                    </svg>
+                    Round {gameData.countRounds() + 1}
                   </button>
                 </div>
               </div>
@@ -291,7 +294,10 @@ function Game() {
             </div>
             <div class="mt-4">
               <button class="btn btn-sm btn-primary" onClick={undoRound}>
-                - Round {gameData.countRounds()}
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                </svg>
+                Round {gameData.countRounds()}
               </button>
             </div>
           </Show>
