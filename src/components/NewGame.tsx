8a3621935fe0cd7c081380756e94536f227d3b48
Barry McMillan <barry.mcmillan@gmail.com>
2024-10-09 17:20:56 +0100

    Fix player placeholder text

--- src/components/NewGame.tsx
+++ src/components/NewGame.tsx
@@ -1,15 +1,17 @@
 import { useNavigate } from "@solidjs/router";
-import { createSignal, Index, Show } from "solid-js";
+import { Index, Show } from "solid-js";
+import { createStore } from "solid-js/store";
 import { createGame } from "../api/GameClient";
 
 const NewGame = () => {
 
     const navigate = useNavigate();
 
-    const [players, setPlayers] = createSignal(['', '', '', '']);
+    const [players, setPlayers] = createStore(['', '', '', '']);
 
     const onSubmit = async () => {
-        const id = await createGame();
+        const arr = players.map((player, index) => player || playerPlaceholder(index));
+        const id = await createGame(arr);
         if (id) {
             navigate(`/game/${id}`);
         }
@@ -17,15 +19,25 @@ const NewGame = () => {
     };
 
     const addPlayer = () => {
-        const arr = [...players()];
-        arr.push('Player ' + (arr.length + 1));
+        const arr = [...players];
+        arr.push(playerPlaceholder(arr.length));
         setPlayers(arr);
     }
 
     const removePlayer = () => {
-        const arr = [...players()];
+        const arr = [...players];
         arr.pop();
         setPlayers(arr);
+    }
+
+    const handleInput = (e: InputEvent) => {
+        const { name, value } = e.currentTarget as HTMLInputElement;
+        const index = parseInt(name.split('-')[1])
+        setPlayers(index, value);
+    }    
+
+    const playerPlaceholder = (forIndex: number) : string => {
+        return "Player " + (forIndex + 1);
     }
 
     return (
@@ -35,13 +47,13 @@ const NewGame = () => {
             </form>
             <h3 class="text-lg font-bold pb-4">New Game</h3>
             <label class="mt-4">Who's playing?</label>
-            <Index each={players()}>
+            <Index each={players}>
                 {(player, index) => (
                     <div class="py-2">
                         <div class="flex flex-row items-center">
                             <div class="text-lg font-bold px-4">{index + 1}</div>
-                            <input type="text" placeholder={"Player " + (index + 1)} class="input input-bordered w-64" />
-                            <Show when={index === players().length - 1}>
+                            <input type="text" placeholder={playerPlaceholder(index)} class="input input-bordered w-64" name={"player-" + index} id={"player-" + index} oninput={handleInput}/>
+                            <Show when={index === players.length - 1}>
                                 <div class="flex flex-row items-center">
                                     <div class="ml-4">
                                         <button class="btn btn-circle btn-sm" onClick={addPlayer}>
@@ -50,7 +62,7 @@ const NewGame = () => {
                                             </svg>
                                         </button>
                                     </div>
-                                    <Show when={players().length > 1}>
+                                    <Show when={players.length > 1}>
                                         <div class="px-2">
                                             <button class="btn btn-circle btn-sm" onClick={removePlayer}>
                                                 <svg class="w-2 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
