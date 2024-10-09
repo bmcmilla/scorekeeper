199472263de7efb4006f85cc459657a611170508
Barry McMillan <barry.mcmillan@gmail.com>
2024-10-09 12:01:25 +0100

    Fix new game placeholders

--- src/components/NewGame.tsx
+++ src/components/NewGame.tsx
@@ -6,9 +6,7 @@ const NewGame = () => {
 
     const navigate = useNavigate();
 
-    const DEFAULT_PLAYER_NAMES = ['Player 1', 'Player 2', 'Player 3', 'Player 4'];
-
-    const [players, setPlayers] = createSignal(DEFAULT_PLAYER_NAMES);
+    const [players, setPlayers] = createSignal(['', '', '', '']);
 
     const onSubmit = async () => {
         const id = await createGame();
@@ -42,19 +40,19 @@ const NewGame = () => {
                     <div class="py-2">
                         <div class="flex flex-row items-center">
                             <div class="text-lg font-bold px-4">{index + 1}</div>
-                            <input type="text" placeholder={DEFAULT_PLAYER_NAMES[index]} class="input input-bordered w-64" />
+                            <input type="text" placeholder={"Player " + (index + 1)} class="input input-bordered w-64" />
                             <Show when={index === players().length - 1}>
                                 <div class="flex flex-row items-center">
-                                    <div>
-                                        <button class="btn btn-circle btn-sm btn-info" onClick={addPlayer}>
+                                    <div class="ml-4">
+                                        <button class="btn btn-circle btn-sm" onClick={addPlayer}>
                                             <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
                                             </svg>
                                         </button>
                                     </div>
                                     <Show when={players().length > 1}>
-                                        <div class="px-2 ">
-                                            <button class="btn btn-circle btn-sm btn-secondary" onClick={removePlayer}>
+                                        <div class="px-2">
+                                            <button class="btn btn-circle btn-sm" onClick={removePlayer}>
                                                 <svg class="w-2 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
                                                 </svg>
