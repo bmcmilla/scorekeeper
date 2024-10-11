ee5a4405f54ea239ff0b6564f6568b5db472dd6b
Barry McMillan <barry.mcmillan@gmail.com>
2024-10-11 12:01:47 +0100

    Add tooltip to delete button

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -287,12 +287,12 @@ function Game() {
                       {(roundIndex) => (
                         <Show when={roundIndex == reversedRounds().length - 1} fallback={<th></th>}>
                           <td>
-                          <div class="tooltip" data-tip={"Delete Round " + (roundIndex + 1)}>
-                            <button class="btn btn-xs" onClick={undoRound}>
-                              <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                              </svg>
-                            </button>
+                            <div class="tooltip" data-tip={"Delete Round " + (roundIndex + 1)}>
+                              <button class="btn btn-xs" onClick={undoRound}>
+                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                </svg>
+                              </button>
                             </div>
                           </td>
                         </Show>
