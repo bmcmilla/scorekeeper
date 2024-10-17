53fa5c34af7f6c3227edfa72ec569029ce50d909
Barry McMillan <barry.mcmillan@gmail.com>
2024-10-17 14:30:50 +0100

    Consolidate Google sign in components

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,17 +1,39 @@
 import { Component } from "solid-js";
-import GoogleOneTap from "./GoogleOneTap";
+import { supabase } from "../api/SupabaseClient";
 
-const SignIn : Component = () => {
+const SignIn: Component = () => {
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const handleCredentialResponse = (params) => {
-        console.log(params);
+    async function handleCredentialResponse(response) {
+        const { data, error } = await supabase.auth.signInWithIdToken({
+            provider: 'google',
+            token: response.credential,
+        });
+
+        if (!error) {
+            console.log(data);
+        }
+
+        console.log(error);
     }
 
     return (
         <div class="flex flex-col items-center m-8 h-dvh">
-            Hello!
-            <GoogleOneTap />
+            <div id="g_id_onload"
+                data-client_id="320191205718-uisumpne4juup798ts2c3lts18f0gngq.apps.googleusercontent.com"
+                data-context="signin"
+                data-ux_mode="popup"
+                data-callback="handleCredentialResponse"
+                data-itp_support="true">
+            </div>
+            <div class="g_id_signin"
+                data-type="standard"
+                data-shape="rectangular"
+                data-theme="filled_black"
+                data-text="signin_with"
+                data-size="large"
+                data-logo_alignment="left">
+            </div>
         </div>
     )
 }
